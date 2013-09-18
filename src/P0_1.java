a0f50bec4ef58e369a5afc07fcb7e74ef2a94efa
Adam Gerber <adam.gerber@yahoo.com>
2013-09-18 13:28:08 -0500

    create pseudocode for P0_1

---
+++ src/P0_1.java
@@ -0,0 +1,29 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ Name your source files according to its Programming Exercise identifier, except replace the period with underscore.
+ For example, if the Programming Exercise identifier is P1.15, then name your java source file P1_15
+ */
+public class P0_1 {
+
+    public static void main(String[] args) {
+
+        //Example: Read each line of a textfile and print it to the console; prepended with a line number
+
+        //pseudocode
+        //Prompt user for absolute path to file
+        //Open file for reading
+        //declare integer counter and initialize to 1
+        //while file has a next line
+            //print the line number to console, then increment counter
+            //print the line content to console + newline
+
+
+    }
+
+
+
+}
