8ed787ac9733cf5bd7a43a0fba11a063a037bc32
Eric Meschke <ericmeschke@gmail.com>
2013-10-12 22:50:18 -0500

    no message

--- src/P5_8.java
+++ src/P5_8.java
@@ -22,7 +22,7 @@ public class P5_8 {
         //Prompt user to input a line to scramble.
         System.out.println("Please enter a line to scramble:");
 
-        //Read in the line word by word and send it to th scramble method.
+        //Read in the line and send it to th scramble method.
         Scanner in = new Scanner(System.in);
         String[] sWord = in.nextLine().split(" ");
 
@@ -33,19 +33,21 @@ public class P5_8 {
     }
     public static String scramble(String word){
         //if word length is < 4 return word.
-        if (word.length() <4){
+        if (word.length() <5){
             return word;
         }
 
-        //else, generate two random numbers.  Multiply those by word length -2 and add 2 to represent the middle
+        //else, generate two random numbers.  Multiply those by word length -3 and add 2 to represent the middle
         //letters of the words.  Flip those two letter in a new array and return that array.
-        int flip1 = (int) (Math.random()*(word.length()-2)+1);
+        //System.out.println(word.length());
+        int flip1 = (int)(Math.random()*(word.length()-3)+1);
         int flip2 = 0;
         do{
-            flip2 = (int) (Math.random()*(word.length()-2)+1);
+            flip2 = (int) (Math.random()*(word.length()-3)+1);
+            //System.out.println("Randomizing");
         } while(flip1 == flip2);
 
-        //System.out.println(flip1 +" "+ flip2);
+        //System.out.print(flip1 +" "+ flip2);
         //Create a new array to flip, flip the letters and return the new array.
 
         char[] cFlip = word.toCharArray();
